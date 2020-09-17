8f50687e2a8ab208a03e2c6db05ebf3a8c9b9f12
Felipe Tomazelli <fefetl08@gmail.com>
2020-09-17 11:07:11 -0300

    feat: coletar nomes de obras

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 from bs4 import BeautifulSoup
+import re
 
 
 with open("example.html", encoding="utf8") as fp:
     soup = BeautifulSoup(fp, features="html.parser")
+
+container = soup.find_all("div", "container pg")
+
+# Nome da obra
+
+nomeObras = []
+for nObra in container:
+    obra = nObra.table.b.next_sibling
+    obra = obra[2:].strip()
+    obra = re.sub(r"\n| {2,}", " ", obra)
+    nomeObras.append(obra)
