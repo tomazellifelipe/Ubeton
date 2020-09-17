27dd20fda91d8b4360d4c427dfe6ed9b326ad931
Felipe Tomazelli <fefetl08@gmail.com>
2020-09-17 11:48:51 -0300

    feat: coletar endereço de obras

--- main.py
+++ main.py
@@ -7,11 +7,18 @@ with open("example.html", encoding="utf8") as fp:
 
 container = soup.find_all("div", "container pg")
 
-# Nome da obra
-
+# Nome das obras
 nomeObras = []
-for nObra in container:
-    obra = nObra.table.b.next_sibling
+for iObra in container:
+    obra = iObra.find("b", string="Nome da Obra").next_sibling
     obra = obra[2:].strip()
     obra = re.sub(r"\n| {2,}", " ", obra)
     nomeObras.append(obra)
+
+# Endereço das obras
+enderecoObras = []
+for iEndereco in container:
+    endereco = iEndereco.find("b", string="Endereço").next_sibling
+    endereco = endereco[2:].strip()
+    endereco = re.sub(r"\n| {2,}", " ", endereco)
+    enderecoObras.append(endereco)
