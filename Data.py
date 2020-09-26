b66f07317837e64a6598f734fa0f26cc749b8ae2
Felipe Tomazelli <fefetl08@gmail.com>
2020-09-26 11:35:37 -0300

    refactor: melhoria dos metodos

--- Data.py
+++ Data.py
@@ -5,21 +5,29 @@ class Obra:
         self.estado = None
         self.cep = None
         self.estagio = None
-
-    def parse(self):
-        return self.__dict__
-
-
-class Construtora:
-    def __init__(self):
-        self.nome = None
-        self.email = None
+        self.nomeCon = None
+        self.emailCon = None
         self.engenheiro = None
         self.telEngenheiro = None
         self.emailEngenheiro = None
         self.comprador = None
         self.telComprador = None
         self.emailComprador = None
+        self.data = []
+        self.index = len(self.data)
 
-    def parse(self):
-        return self.__dict__
+    def parseData(self):
+        self.data = [self.nome, self.endereco, self.estado, self.cep,
+                     self.estagio, self.nomeCon, self.emailCon,
+                     self.engenheiro, self.telEngenheiro, self.emailEngenheiro,
+                     self.comprador, self.telComprador, self.emailComprador]
+        self.index = len(self.data)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index == 0:
+            raise StopIteration
+        self.index = self.index - 1
+        return self.data[self.index]
--- main.py
+++ main.py
@@ -3,182 +3,93 @@ import re
 import pandas as pd
 from Data import Obra
 
-with open("example.html", encoding="utf8") as fp:
-    soup = BeautifulSoup(fp, features="html.parser")
 
-containers = soup.find_all("div", "container pg")  # raíz dos blocos
-for path in containers:
-    # DADOS DA OBRA PATH
-    obra = Obra()
-    dadosDaObraPath = path.find("td", style="width:85% !important;")
-    nomeDaObra = dadosDaObraPath.find("b", string="Nome da Obra").next_sibling
-    enderecoDaObra = dadosDaObraPath.find("b", string="Endereço").next_sibling
-    ufDaObra = dadosDaObraPath.find("b", string="Estado:").next_sibling
-    cepDaObra = dadosDaObraPath.find("b", string="CEP:").next_sibling
-    estagiodaObra = dadosDaObraPath.find("b", string="Estágio").next_sibling
+def htmlUbeton(fileName):
+    with open(fileName, encoding="utf8") as fp:
+        soup = BeautifulSoup(fp, features="html.parser")
 
-    # CONTADOS DA OBRA
-    contatosPath = path.find("b", string="CONTATO(s) DA OBRA").find_parent(
-        "p").find_next_sibling("table")
-    engPath = contatosPath.find("td", string="ENG. RESPONSÁVEL OBRA")
-    if engPath is None:
-        nomeDoEng = None
-        telDoEng = None
-        emailDoEng = None
-    else:
-        nomeDoEng = engPath.find_previous_sibling("td").string
-        engPath_tr = engPath.find_parent("tr")
-        telDoEng = engPath_tr.find(
-            string=re.compile("\(\d{2}\) \d{4}-\d{4}"))
-        emailDoEng = engPath_tr.find(
-            string=re.compile("@|restrito"))
-    compradorPath = contatosPath.find("td", string="COMPRADOR / MATERIAIS")
-    if engPath is None:
-        nomeDoComprador = None
-        telDoComprador = None
-        emailDoComprador = None
-    else:
-        nomeDoComprador = compradorPath.find_previous_sibling("td").string
-        telDoComprador = compradorPath.find_parent("tr").find(
-            string=re.compile("\(\d{2}\) \d{4}-\d{4}"))
-        emailDoComprador = compradorPath.find_parent("tr").find(
-            string=re.compile("@|restrito"))
-    print(contatosPath)
+    containers = soup.find_all("div", "container pg")  # raíz dos blocos
 
+    todasAsObras = []
+    for path in containers:
+        # DADOS DA OBRA PATH
+        obra = Obra()
+        dadosDaObraPath = path.find(
+            "td", style="width:85% !important;")
+        obra.nome = dadosDaObraPath.find(
+            "b", string="Nome da Obra").next_sibling
+        obra.endereco = dadosDaObraPath.find(
+            "b", string="Endereço").next_sibling
+        obra.estado = dadosDaObraPath.find(
+            "b", string="Estado:").next_sibling
+        obra.cep = dadosDaObraPath.find(
+            "b", string="CEP:").next_sibling
+        obra.estagio = dadosDaObraPath.find(
+            "b", string="Estágio").next_sibling
 
-# Nome das obras
-nomeObras = []
-enderecoObras = []
-ufObras = []
-cepObras = []
-estagioObras = []
-nomeConstrutora = []
-emailConstrutora = []
-nomeEngenheiro = []
-telEngenheiro = []
-emailEngenheiro = []
-nomeComprador = []
-telComprador = []
-emailComprador = []
+        # CONTADOS DA OBRA
+        contatosDaObraPath = path.find(
+            "b", string="CONTATO(s) DA OBRA").find_parent(
+            "p").find_next_sibling("table")
+        engenheiroPath = contatosDaObraPath.find(
+            "td", string="ENG. RESPONSÁVEL OBRA")
 
-for iObra in containers:
-    obra = iObra.find("b", string="Nome da Obra").next_sibling
-    obra = obra[2:].strip()
-    obra = re.sub(r"\n| {2,}", " ", obra)
-    nomeObras.append(obra)
-
-# Endereço das obras
-for iEndereco in containers:
-    endereco = iEndereco.find("b", string="Endereço").next_sibling
-    endereco = endereco[2:].strip()
-    endereco = re.sub(r"\n| {2,}", " ", endereco)
-    enderecoObras.append(endereco)
-
-# Endereço: uf das obras
-for iufObra in containers:
-    uf = iufObra.find("b", string="Estado:").next_sibling
-    uf = uf.strip()
-    uf = re.sub(r"\n| -", "", uf)
-    ufObras.append(uf)
-
-# Endereço: cep das obras
-for icepObra in containers:
-    cep = icepObra.find("b", string="CEP:").next_sibling
-    cep = cep.strip()
-    cep = re.sub(r"\n| {1,}", "", cep)
-    cepObras.append(cep)
-
-# Estado das obras
-for iEstagio in containers:
-    estagio = iEstagio.find("b", string="Estágio").next_sibling
-    estagio = estagio[2:].strip()
-    estagioObras.append(estagio)
-
-# ----------------------------------------------------------------
-# Nome da construtora
-for iConstrutora in containers:
-    construtora = iConstrutora.find(
-        "b", string="CONSTRUÇÃO CIVIL").next_sibling
-    construtora = construtora[3:].strip()
-    nomeConstrutora.append(construtora)
-
-# E-mail da construtora
-for iConstrutora in containers:
-    emailConstr = iConstrutora.find("b", string="Site:").next_sibling
-    emailConstr = emailConstr.strip()
-    emailConstrutora.append(emailConstr)
-
-
-# ----------------------------------------------------------------
-# Nome do engenheiro responsável
-for iEngenheiro in containers:
-    procuraDado = iEngenheiro.find(string="ENG. RESPONSÁVEL OBRA")
-    if procuraDado is None:
-        nomeEngenheiro.append("Sem dado")
-    else:
-        newPath = procuraDado.find_parent("tr")
-        engenheiro = newPath.td.string
-        nomeEngenheiro.append(engenheiro)
-
-# Telefone do engenheiro responsável
-for iTelEngenheiro in containers:
-    procuraDado = iTelEngenheiro.find(string="ENG. RESPONSÁVEL OBRA")
-    if procuraDado is None:
-        telEngenheiro.append("Sem dado")
-    else:
-        newPath = procuraDado.find_parent("tr")
-        telEng = newPath.find(string=re.compile("\(\d{2}\) \d{4}-\d{4}"))
-        telEngenheiro.append(telEng)
-
-# E-mail do engenheiro responsável
-for iemailEngenheiro in containers:
-    procuraDado = iemailEngenheiro.find(string="ENG. RESPONSÁVEL OBRA")
-    if procuraDado is None:
-        emailEngenheiro.append("Sem dado")
-    else:
-        newPath = procuraDado.find_parent("tr")
-        emailEng = newPath.find(string=re.compile("@|restrito"))
-        if emailEng is None:
-            emailEngenheiro.append("Sem dado")
+        if engenheiroPath is None:
+            obra.engenheiro = None
+            obra.telEngenheiro = None
+            obra.emailEngenheiro = None
         else:
-            emailEngenheiro.append(emailEng)
+            obra.eng = engenheiroPath.find_previous_sibling("td").string
+            engPath_tr = engenheiroPath.find_parent("tr")
+            obra.telEngenheiro = engPath_tr.find(
+                string=re.compile("\(\d{2}\) \d{4}-\d{4}"))
+            obra.emailEngenheiro = engPath_tr.find(
+                string=re.compile("@|restrito"))
 
-# ----------------------------------------------------------------
-# Nome do comprador
-for iComprador in containers:
-    procuraDado = iComprador.find(string="COMPRADOR / MATERIAIS")
-    if procuraDado is None:
-        nomeComprador.append("Sem dado")
-    else:
-        newPath = procuraDado.find_parent("tr")
-        comprador = newPath.td.string
-        nomeComprador.append(comprador)
+        compradorPath = contatosDaObraPath.find(
+            "td", string="COMPRADOR / MATERIAIS")
 
-# Telefone do comprador
-for iComprador in containers:
-    procuraDado = iComprador.find(string="COMPRADOR / MATERIAIS")
-    if procuraDado is None:
-        telComprador.append("Sem dado")
-    else:
-        newPath = procuraDado.find_parent("tr")
-        telCompra = newPath.find(string=re.compile("\(\d{2}\) \d{4}-\d{4}"))
-        telComprador.append(telCompra)
-
-
-# E-mail do comprador
-for iComprador in containers:
-    procuraDado = iComprador.find(string="COMPRADOR / MATERIAIS")
-    if procuraDado is None:
-        emailComprador.append("Sem dado")
-    else:
-        newPath = procuraDado.find_parent("tr")
-        emailCompra = newPath.find(string=re.compile("@|restrito"))
-        if emailEng is None:
-            emailComprador.append("Sem dado")
+        if compradorPath is None:
+            obra.comprador = None
+            obra.telComprador = None
+            obra.emailComprador = None
         else:
-            emailComprador.append(emailCompra)
+            obra.comprador = compradorPath.find_previous_sibling(
+                "td").string
+            obra.telComprador = compradorPath.find_parent(
+                "tr").find(
+                string=re.compile("\(\d{2}\) \d{4}-\d{4}"))
+            obra.emailComprador = compradorPath.find_parent(
+                "tr").find(
+                string=re.compile("@|restrito"))
 
-# ------------------------------------------------
+        obra.nomeCon = path.find(
+            "b", string="CONSTRUÇÃO CIVIL").next_sibling
+        obra.emailCon = path.find(
+            "b", string="Site:").next_sibling
+        obra.parseData()
+        todasAsObras.append(obra)
+
+    return todasAsObras
+
+
+lista = htmlUbeton("example.html")
+
+
+def dictUbeton(listaDeObras):
+    dictObras = {"Obra": [], "Endereço": [], "UF": [], "CEP": [],
+                 "Estágio": [], "Construtora": [], "Email": [],
+                 "Engenheiro": [], "Telefone-Eng": [], "Email-Eng": [],
+                 "Comprador": [], "Telefone-Comp": [], "Email-Comp": []}
+    for obra in listaDeObras:
+        for key, value in zip(dictObras.keys(), obra.data):
+            dictObras[key].append(value)
+    return pd.DataFrame(dictObras)
+
+
+print(dictUbeton(lista))
+
+"""# ------------------------------------------------
 # PANDAS E EXCEL
 # ------------------------------------------------
 data = {"Nome da Obra": nomeObras, "Endereço da Obra": enderecoObras,
@@ -188,4 +99,4 @@ data = {"Nome da Obra": nomeObras, "Endereço da Obra": enderecoObras,
         "E-mail Eng.": emailEngenheiro, "Comprador": nomeComprador,
         "Tel Comprador": telComprador, "E-mail Comprador": emailComprador}
 
-dt = pd.DataFrame(data).to_excel("Dados_Intec2.xlsx")
+dt = pd.DataFrame(data).to_excel("Dados_Intec2.xlsx")"""
